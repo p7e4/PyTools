108fccfaae3d673b218330ebba4e9e821c661a24
p7e4 <44750087+p7e4@users.noreply.github.com>
2020-10-03 20:46:44 +0800

    Create dd.py

---
+++ dd.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sys, re
+
+def getDictList(dict):
+    regx = '''[\w\~`\!\@\#\$\%\^\&\*\(\)\_\-\+\=\[\]\{\}\:\;\,\.\/\<\>\?]+'''
+    with open(dict) as f:
+        data = f.read()
+        return re.findall(regx, data)
+
+def rmdp(dictList):
+    return list(set(dictList))
+
+def fileSave(dictRmdp, out):
+    with open(out, 'a') as f:
+        for line in dictRmdp:
+            f.write(line + '\n')
+
+def main():
+    try:
+        dict = sys.argv[1].strip()
+        out = sys.argv[2].strip()
+    except Exception, e:
+        me = sys.argv[0]
+        print('usage: %s <input> <output>' %me)
+        print('example: %s dict.txt dict_rmdp.txt' %me)
+        exit()
+
+    dictList = getDictList(dict)
+    dictRmdp = rmdp(dictList)
+    fileSave(dictRmdp, out)
+
+if __name__ == '__main__':
+    main()
